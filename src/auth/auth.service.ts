996e809f41e6cb0a70f75f58a51923c65c510335
jumper2210 <47790273+jumper2210@users.noreply.github.com>
2022-12-11 20:05:04 +0100

    Updated migration

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,12 +2,32 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { ForbiddenException } from '@nestjs/common/exceptions';
 
 @Injectable()
 export class AuthService {
-  constructor(private authService: PrismaService) {}
-  signUp(dto: AuthDto) {
-    return { msg: 'test signup' };
+  constructor(private prisma: PrismaService) {}
+  async signUp(dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+      delete user.hash;
+
+      return user;
+    } catch (err) {
+      if (err instanceof PrismaClientKnownRequestError) {
+        if (err.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+      throw err;
+    }
   }
   singIn() {
     return { msg: 'test login' };
