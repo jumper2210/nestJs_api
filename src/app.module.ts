eb96e7ff340d05bfbf7f2b37dd175e9510217290
jumper2210 <47790273+jumper2210@users.noreply.github.com>
2022-12-11 19:07:12 +0100

    Changed typeorm with prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,25 +3,17 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { ConfigModule } from '@nestjs/config';
-import * as Joi from '@hapi/joi';
-import { DatabaseModule } from './database/database.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     AuthModule,
     UserModule,
     BookmarkModule,
-    ConfigModule.forRoot({
-      validationSchema: Joi.object({
-        POSTGRES_HOST: Joi.string().required(),
-        POSTGRES_PORT: Joi.number().required(),
-        POSTGRES_USER: Joi.string().required(),
-        POSTGRES_PASSWORD: Joi.string().required(),
-        POSTGRES_DB: Joi.string().required(),
-        PORT: Joi.number(),
-      }),
-    }),
-    DatabaseModule,
+    PrismaModule,
   ],
 })
 export class AppModule {}
