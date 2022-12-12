ee7a19e8b52fa326f1f0232ac94e9600b8c4680f
jumper2210 <47790273+jumper2210@users.noreply.github.com>
2022-12-12 13:17:54 +0100

    Added get-user decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Post } from '@nestjs/common';
-import { Body } from '@nestjs/common/decorators';
+import { Controller, HttpStatus, Post } from '@nestjs/common';
+import { Body, HttpCode } from '@nestjs/common/decorators';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 
@@ -11,6 +11,8 @@ export class AuthController {
   signUp(@Body() dto: AuthDto) {
     return this.authService.signUp(dto);
   }
+
+  @HttpCode(HttpStatus.OK)
   @Post('signin')
   signIn(@Body() dto: AuthDto) {
     return this.authService.singIn(dto);
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,15 @@
 import { Controller } from '@nestjs/common';
 import { UseGuards } from '@nestjs/common/decorators/core/use-guards.decorator';
 import { Get } from '@nestjs/common/decorators/http/request-mapping.decorator';
-import { AuthGuard } from '@nestjs/passport';
+import { User } from '@prisma/client';
+import { GetUser } from 'src/auth/decorator';
+import { JwtGuard } from 'src/auth/guard';
 
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
-  @UseGuards(AuthGuard('jwt'))
   @Get('me')
-  getMe() {
-    return 'user info';
+  getMe(@GetUser() user: User) {
+    return user;
   }
 }
